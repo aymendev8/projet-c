82c1bb01c7e37e73d51726bb03a8553ee89da025
Aymen Kadri <aymenkadri5@gmail.com>
2022-11-15 09:14:23 +0100

    debut du txt enrichi

---
+++ p_texte_enrichi.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "p_texte_enrichi.h"
+
+t_mode mon_mode = NORMAL;
+
+void changer_mode(t_mode mode)
+{
+    mon_mode = mode;
+}
+
+t_mode mode()
+{
+    return mon_mode;
+}
+
+void ouvrir_bloc()
+{
+    printf("+");
+    for (int i = 0; i < 79; i++)
+        printf("-");
+}
