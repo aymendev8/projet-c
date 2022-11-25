e74334e6d8fde0b86a13b6691e7160c31f7a37ac
aymendev8 <aymenkadri5@gmail.com>
2022-11-25 21:13:58 +0100

    parti 1 fini

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include "p_texte_enrichi.c"
-#include "p_texte_enrichi.h"
 
 int main()
 {
@@ -9,10 +8,15 @@ int main()
     ecrire_mot("tout");
     ecrire_mot("le");
     ecrire_mot("monde");
-    terminer_ligne();
+    ecrire_mot("anticonstitutionnellement");
+    ecrire_mot("anticonstitutionnellement");
+    ecrire_mot("anticonstitutionnellement");
     ouvrir_bloc();
-    ouvrir_bloc();
-    fermer_bloc();
+    entamer_ligne();
+    ecrire_mot("Bonjour");
+    ecrire_mot("anticonstitutionnellement");
+    ecrire_mot("anticonstitutionnellement");
+    ecrire_mot("anticonstitutionnellement");
     fermer_bloc();
     fermer_bloc();
     return 0;
